e7dadf8a1db58e1fd7f1a076d411ed0b6f24a555
fabiodrneles <fabiodrneles@gmail.com>
2023-09-08 19:31:10 -0300

    terminando o grid

--- frontend/src/components/Grid.js
+++ frontend/src/components/Grid.js
@@ -15,11 +15,32 @@ const Table = styled.table`
     word-break: break-all;
 `;
 
+export const Thead = styled.thead``;
 
+export const Tr = styled.tr``;
+
+export const Th = styled.th`
+    text-align: start;
+    border-bottom: inset;
+    padding-bottom: 5px;
+
+    @media (max-width: 500px) {
+        ${(props) => props.onlyWeb && "display: none"}
+    }
+`;
 
 const Grid = () => {
     return (
         <Table>
+            <Thead>
+                <Tr>
+                    <Th>Name</Th>
+                    <Th>Email</Th>
+                    <Th onlyWeb>Fone</Th>
+                    <Th></Th>
+                    <Th></Th>
+                </Tr>
+            </Thead>
         </Table>
     );
 };
