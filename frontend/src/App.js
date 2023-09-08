db2de3fb5b25e943c1785135c7876a01b030eab8
fabiodrneles <fabiodrneles@gmail.com>
2023-09-08 13:49:27 -0300

    importando recursos nos arquivos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import GlobalStyle from "./styles/global";
+import styled from "styled-components";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 
 function App() {
 
   return (
     <>
+      <Container>
+        
+      </Container>
+      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
       <GlobalStyle />
 
     </>
